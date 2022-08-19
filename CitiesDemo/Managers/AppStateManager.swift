ba95dca73cc7ec1bba3d8853e15efc0690aea8ba
Jeddawy <>
2022-08-19 16:22:25 +0200

    [ADD] AppState Manager

---
+++ CitiesDemo/Managers/AppStateManager.swift
@@ -0,0 +1,67 @@
+//
+//  AppStateManager.swift
+//  CitiesDemo
+//
+//  Created by Geddawy on 19/08/2022.
+//
+
+import Foundation
+import UIKit
+
+protocol AppStateManagerProtocol {
+    func lunch(appDelegate: AppDelegateProtocol)
+}
+
+class AppStateManager {
+    
+    // MARK:- AppState Enum
+    enum AppState {
+        case home
+        case none
+    }
+    
+    // MARK:- Properties
+    var appDelegate: AppDelegateProtocol!
+    var mainWindow: UIWindow? {
+        return self.appDelegate?.getMainWindow()
+    }
+    
+    var state: AppState = .none {
+        willSet(newState) {
+            switch newState {
+            case .home:
+                self.switchToHomeState()
+            default:
+                break
+            }
+        }
+    }
+    
+    // MARK:- Singleton
+    private static let sharedInstance = AppStateManager()
+    
+    class func shared() -> AppStateManagerProtocol {
+        return AppStateManager.sharedInstance
+    }
+    
+    // MARK:- Methods
+
+
+    func switchToHomeState() {
+        let citiesVC = CitiesViewController.create()
+        self.mainWindow?.rootViewController = citiesVC
+    }
+    
+}
+
+extension AppStateManager: AppStateManagerProtocol {
+    func lunch(appDelegate: AppDelegateProtocol) {
+        self.appDelegate = appDelegate
+        self.state = .home
+    }
+    
+    func start(){
+        print("state is",state)
+    }
+    
+}
