462c4c399affa5d898ef38f94485dbfc8ff62504
Jeddawy <>
2022-08-19 16:22:39 +0200

    [Add] Contants file

---
+++ CitiesDemo/Utilities/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  CitiesDemo
+//
+//  Created by Geddawy on 19/08/2022.
+//
+
+import Foundation
+
+struct URLs{
+    static let base = "http://assignment.pharos-solutions.de/"
+    static let cities = "cities.json"
+}
+
+struct GoogleAPIKey{
+    static let Key = "AIzaSyDp1TeAL54m5bl6wCFIs4RZj4EPnvElR7g"
+//    AIzaSyDp1TeAL54m5bl6wCFIs4RZj4EPnvElR7g
+    //AIzaSyBGHUxAus7hBBoE0yewVf1np6pCpblG4no
+}
