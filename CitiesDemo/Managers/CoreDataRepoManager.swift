2aa11077d01798856e58ff0591188b51aac7576b
Jeddawy <>
2022-08-20 19:57:19 +0200

    [Modify] fetch data from coreDataManager with batches, and add method gets counts

--- CitiesDemo/Managers/CoreDataRepoManager.swift
+++ CitiesDemo/Managers/CoreDataRepoManager.swift
@@ -28,11 +28,45 @@ class CoreDataRepository<T: NSManagedObject> {
     }
     
     @discardableResult
+    func getBatched(fetchOffSet: Int, fetchLimit:Int ) -> [T]? {
+        let EntityName = getEntityName(type: T.self)
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: EntityName)
+        request.fetchOffset = fetchOffSet
+        request.fetchLimit = fetchLimit
+        let objects = try? context!.fetch(request) as? [T]
+        return objects
+    }
+    
+    @discardableResult
     func getAll() -> [T]? {
         let EntityName = getEntityName(type: T.self)
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: EntityName)
         let objects = try? context!.fetch(request) as? [T]
         return objects
+    }
+    
+    @discardableResult
+    func getCount() -> Int? {
+        let EntityName = getEntityName(type: T.self)
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: EntityName)
+        request.resultType = .countResultType
+        let count = try? context!.count(for: request)
+        return count
+    }
+    
+    
+    @discardableResult
+    func register(value: T) -> T?{
+        
+        var result = false
+        do {
+            try context!.save()
+            result = true
+        } catch _ as NSError {
+         result = false
+        }
+        return result == true ? value : nil
+        
     }
     
     @discardableResult
